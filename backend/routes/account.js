8a86b37e34b73e7f8e314f29e568e6b63584f92c
Nitesh-thapliyal <niteshnits.thapliyal@gmail.com>
2024-07-04 23:15:15 +0530

    added money transfer route with transaction

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -14,6 +14,42 @@ router.get("/balance", authMiddleware, async(req, res) => {
     })
 });
 
+router.post("/transfer", authMiddleware, async(req,res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const {amount, to} = req.body;
+
+    // Fetch accounts within the transaction
+    const account = Account.findOne({ userId: req.userId }).session(session);
+
+    if(!account || account.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient Balance"
+        });
+    }
+
+    const toAccount = await Account.findOne({userId: to}).session(session);
+
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid account"
+        });
+    }
+
+    // Perform a transfer
+    await Account.updateOne({userId: req.userId}, {$inc: {balance: -amount}}).session(session);
+    await Account.updateOne({userId: to}, {$inc: {balance: amount}}).session(session);
+
+    // commit the transaction
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    });
+});
+
 
 module.exports = router;
 
